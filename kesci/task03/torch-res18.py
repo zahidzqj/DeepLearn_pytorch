25096807ba2bcbb40eba4ceb52c7bf06e261d71f
zahidzqj <37623070+zahidzqj@users.noreply.github.com>
2020-04-10 23:55:37 +0800

    Add files via upload

---
+++ kesci/task03/torch-res18.py
@@ -0,0 +1,200 @@
+#import
+import sys
+sys.path.append("/home/kesci/input")
+#import d2lzh1981 as d2l
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import time
+import torchvision
+
+class Flatten(torch.nn.Module):  #展平操作
+    def forward(self, x):
+        return x.view(x.shape[0], -1)
+
+class Reshape(torch.nn.Module): #将图像大小重定型
+    def forward(self, x):
+        return x.view(-1,1,28,28)      #(B x C x H x W)
+    
+class ResidualBlock(nn.Module):   # 我们定义网络时一般是继承的torch.nn.Module创建新的子类
+    def __init__(self, inchannel, outchannel, stride=1):
+        super(ResidualBlock, self).__init__()
+        #torch.nn.Sequential是一个Sequential容器，模块将按照构造函数中传递的顺序添加到模块中。
+        self.left = nn.Sequential(
+            nn.Conv2d(inchannel, outchannel, kernel_size=3, stride=stride, padding=1, bias=False), 
+            # 添加第一个卷积层,调用了nn里面的Conv2d（）
+            nn.BatchNorm2d(outchannel), # 进行数据的归一化处理
+            nn.ReLU(inplace=True), # 修正线性单元，是一种人工神经网络中常用的激活函数
+            nn.Conv2d(outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(outchannel)
+        )
+        self.shortcut = nn.Sequential() 
+        if stride != 1 or inchannel != outchannel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(inchannel, outchannel, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(outchannel)
+            )
+        #  便于之后的联合,要判断Y = self.left(X)的形状是否与X相同
+
+    def forward(self, x): # 将两个模块的特征进行结合，并使用ReLU激活函数得到最终的特征。
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, ResidualBlock, num_classes=10):
+        super(ResNet, self).__init__()
+        self.inchannel = 64
+        self.conv1 = nn.Sequential( # 用3个3x3的卷积核代替7x7的卷积核，减少模型参数
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        ) 
+        self.layer1 = self.make_layer(ResidualBlock, 64,  2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)   #第一个ResidualBlock的步幅由make_layer的函数参数stride指定
+        # ，后续的num_blocks-1个ResidualBlock步幅是1
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+def ResNet18():
+    return ResNet(ResidualBlock)
+
+# 模型定义-ResNet
+net = ResNet18()
+
+
+def load_data_fashion_mnist(batch_size, resize=None, root='./MNIST_data'):
+    """Download the fashion mnist dataset and then load into memory."""
+    trans = []
+    if resize:
+        trans.append(torchvision.transforms.Resize(size=resize))
+    trans.append(torchvision.transforms.ToTensor())
+    
+    transform = torchvision.transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+    if sys.platform.startswith('win'):
+        num_workers = 0  # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_iter, test_iter
+
+
+# 数据
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size, root='./MNIST_data')
+print(len(train_iter))
+'''
+#数据展示
+import matplotlib.pyplot as plt
+def show_fashion_mnist(images, labels):
+    #d2l.use_svg_display()
+    # 这里的_表示我们忽略（不使用）的变量
+    _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.view((28, 28)).numpy())
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+for Xdata,ylabel in train_iter:
+    break
+X, y = [], []
+for i in range(10):
+    print(Xdata[i].shape,ylabel[i].numpy())
+    X.append(Xdata[i]) # 将第i个feature加到X中
+    y.append(ylabel[i].numpy()) # 将第i个label加到y中
+#show_fashion_mnist(X, y)
+'''
+def try_gpu():
+    """If GPU is available, return torch.device as cuda:0; else return torch.device as cpu."""
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
+    return device
+
+device = try_gpu()
+
+
+def evaluate_accuracy(data_iter, net,device=torch.device('cpu')):
+    """Evaluate accuracy of a model on the given data set."""
+    acc_sum,n = torch.tensor([0],dtype=torch.long,device=device),0#float32
+    for X,y in data_iter:
+        # If device is the GPU, copy the data to the GPU.
+        X,y = X.to(device),y.to(device)
+        net.eval()
+        with torch.no_grad():
+            acc_sum += torch.sum((torch.argmax(net(X), dim=1) == y))  #[[0.2 ,0.4 ,0.5 ,0.6 ,0.8] ,[ 0.1,0.2 ,0.4 ,0.3 ,0.1]] => [ 4 , 2 ]
+            n += y.shape[0]
+    return acc_sum.item()/n
+
+#训练函数
+def train_ch5(net, train_iter, test_iter,criterion, num_epochs, batch_size, device,lr=None):
+    """Train and evaluate a model with CPU or GPU."""
+    print('training on', device)
+    net.to(device)
+    optimizer = optim.SGD(net.parameters(), lr=lr)
+    for epoch in range(num_epochs):
+        train_l_sum = torch.tensor([0.0],dtype=torch.float32,device=device)
+        train_acc_sum = torch.tensor([0.0],dtype=torch.float32,device=device)
+        n, start = 0, time.time()
+        for X, y in train_iter:
+            net.train()
+            
+            optimizer.zero_grad()
+            X,y = X.to(device),y.to(device) 
+            y_hat = net(X)
+            loss = criterion(y_hat, y)
+            loss.backward()
+            optimizer.step()
+            
+            with torch.no_grad():
+                y = y.long()
+                train_l_sum += loss.float()
+                train_acc_sum += (torch.sum((torch.argmax(y_hat, dim=1) == y))).float()
+                n += y.shape[0]
+        test_acc = evaluate_accuracy(test_iter, net,device)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, '
+              'time %.1f sec'
+              % (epoch + 1, train_l_sum/n, train_acc_sum/n, test_acc,
+                 time.time() - start))
+
+
+# 训练
+lr, num_epochs = 0.9, 10
+
+def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        torch.nn.init.xavier_uniform_(m.weight)
+
+net.apply(init_weights)
+net = net.to(device)
+
+criterion = nn.CrossEntropyLoss()   #交叉熵描述了两个概率分布之间的距离，交叉熵越小说明两者之间越接近
+train_ch5(net, train_iter, test_iter, criterion,num_epochs, batch_size,device, lr)
